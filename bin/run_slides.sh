b1c1ec342b982ecf79cb8f3f3f4c79981256d320
Gil Brandao <gbrandao@pminds.pt>
2018-02-28 14:31:35 +0000

    adds a command for opening the presentation

---
+++ bin/run_slides.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#
+#
+
+set -euo pipefail
+
+ROOT=$(readlink -f `dirname ${BASH_SOURCE[0]}`/..)
+
+
+function usage () {
+  echo "usage not implemented" >&2
+}
+
+
+while getopts ":h" opt; do
+  case "$opt" in
+    h)
+      usage
+      exit 0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      exit 1
+      ;;
+    :)
+      echo "flag -$OPTARG requires arguments" >&2
+      usage
+      exit 1
+      ;;
+    *)
+      echo "assert error" >&2
+      exit 1
+      ;;
+    esac
+done
+shift $((OPTIND-1))
+
+
+firefox --private-window "$ROOT/slides/public/index.html"
+
+# vi: set ts=2 sw=2 : 
+# EOF
